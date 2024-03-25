2230d796002cba7dc43c0660c6a8ef1c2010ba96
marcomarcovski <161604330+marcomarcovski@users.noreply.github.com>
2024-03-25 09:24:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".botao");
 for(let i=;i<botoes.lenght;i++){
   botoes[i].onclick=function(){
+
+
+    botoes[j].classlist.remove("ativo")
+    textos[j].classlist.remove("ativo")
+  }
+ botoes[j].classlist.add("ativo");
+  textos[j].classlist.add("ativo");
+} 
+}
+  
+    
+const contadores= document.queryselectorall(".contador");
+const tempoObjetivo1= new Date("2024-03-25T00:00:00");
+let tempoAtual= new date();
+contadores[0].textcontent= tempoObjetivo1-tempoAtual;
