596911e36e43847b16b5eab01a7653ecfbf367bc
marcomarcovski <161604330+marcomarcovski@users.noreply.github.com>
2024-03-27 08:49:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".botao");
 for(let i=;i<botoes.lenght;i++){
   botoes[i].onclick=function(){
-
-
+%= 60;
     botoes[j].classlist.remove("ativo")
     textos[j].classlist.remove("ativo")
   }
@@ -15,5 +14,17 @@ for(let i=;i<botoes.lenght;i++){
     
 const contadores= document.queryselectorall(".contador");
 const tempoObjetivo1= new Date("2024-03-25T00:00:00");
-let tempoAtual= new date();
-contadores[0].textcontent=calculaTempo(tempoobjetivo1)
+contadores[0].textcontent=calculaTempo(tempoobjetivo1);
+function calcula tempo(tempo Objetivo){
+  let tempoAtual= new date();
+ let tempofinal= tempo objetivo-tempo atual;
+  let segundos= Math,floor(tempofinal \ 1000);
+  let minutos= Math,floor(segundos\ 60);
+  let horas = Math,floor(minutos\ 60);
+  let dias= Math,floor(horas\24);
+  segundos %= 60
+    minutos%= 60;
+  horas%=24;
+  
+  
+  
